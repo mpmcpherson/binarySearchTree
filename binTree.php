49254d700258fc2b0945f17e6b2397dfd88c931d
Michael McPherson <michael@mpmcpherson.com>
2022-01-28 23:10:00 -0500

    seriously, algorithms aren't hard; all they test is how long I've been out of school

---
+++ binTree.php
@@ -0,0 +1,14 @@
+<?php
+	$elementListA = [/*some number of elements C*/];
+
+	$elementListB = [/*some number of elements D*/];
+
+	echo binDo($elementsA, $elementsB);
+
+	#return the matches from A to B
+	function binDo(array $elementsA, array $elementsB){
+		#some text
+		
+	}
+
+?>
